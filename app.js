9eb1b4db73ad24e9b71bd281097c9b96c727cce1
Anita Safar <ancsa0416@gmail.com>
2021-11-19 16:37:12 +0000

    form submitted as text content

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("Content loaded")
+    const newAnimal = document.querySelector("#form");
+    newAnimal.addEventListener('submit', handleNewAnimal);
+
 });
+
+
+const handleNewAnimal = function(animal) {
+    animal.preventDefault();
+    console.log(animal.target.name.value);
+
+    const animalList = document.querySelector("#animal-list");
+
+    animalList.textContent = `Animal1: ${animal.target.name.value} ${animal.target.select.value} 
+    ${animal.target.population.value} ${animal.target.population.value}`
+};
+
