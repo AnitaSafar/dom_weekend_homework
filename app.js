7f3023b25415688d400143b3bfec0547edb335c6
Anita Safar <ancsa0416@gmail.com>
2021-11-19 17:10:18 +0000

    population extended, habitat added to list item

--- app.js
+++ app.js
@@ -17,17 +17,21 @@ const addAnimalToList = function(form) {
     const animalInList = document.createElement('li');
     animalInList.classList.add("animal-in-list");
 
-    const name = document.createElement("h2");
+    const name = document.createElement("h3");
     name.textContent = form.name.value;
     animalInList.appendChild(name);
 
-    const status = document.createElement("h3");
+    const status = document.createElement("p");
     status.textContent = form.status.value;
     animalInList.appendChild(status);
 
-    const population = document.createElement("h3");
-    population.textContent = form.population.value;
+    const population = document.createElement("p");
+    population.textContent = `Population: ${form.population.value}`;
     animalInList.appendChild(population);
+
+    const habitat = document.createElement("p");
+    habitat.textContent = `Habitat(s): ${form.habitat.value}`;
+    animalInList.appendChild(habitat);
 
     return animalInList;
 }
